f9d8167f80fcd41fcb3600afca53aaba7085087f
Ethan <ethangkshim@outlook.com>
2025-06-27 14:00:47 -0400

    Added routing for all pages

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,8 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Menu from "./components/Menu";
+import Cart from "./components/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
-  return <div className="w-screen h-screen bg-black"></div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/About" element={<About />} />
+          <Route exact path="/Cart" element={<Cart />} />
+          <Route exact path="/Contact" element={<Contact />} />
+          <Route exact path="/Menu" element={<Menu />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ restaurant/src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="w-screen h-screen bg-black"></div>;
+};
+
+export default Home;
